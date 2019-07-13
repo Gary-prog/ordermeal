c2c6499caaa0e1784bb6f7dee38e212634d57ab7
uk403 <1176779086@qq.com>
2019-07-13 09:23:13 +0800

    ukyu

--- src/main/java/com/chinasoftintership/ordermeal/dal/entity/Order.java
+++ src/main/java/com/chinasoftintership/ordermeal/dal/entity/Order.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="order")
+@Table(name="orders")
 public class Order {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
