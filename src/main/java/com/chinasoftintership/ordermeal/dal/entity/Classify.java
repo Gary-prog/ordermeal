1c572669fe8aac76772b08d94094999beb3662bd
uk403 <1176779086@qq.com>
2019-07-15 10:55:44 +0800

    ukyu

--- src/main/java/com/chinasoftintership/ordermeal/dal/entity/Classify.java
+++ src/main/java/com/chinasoftintership/ordermeal/dal/entity/Classify.java
@@ -14,30 +14,8 @@ import javax.persistence.*;
 public class Classify {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int classify_id;//分类编号
+    private int classifyId;//分类编号
     private String classification;//分类名称
 
-//    public int getClassify_id() {
-//        return classify_id;
-//    }
-//
-//    public String getClassification() {
-//        return classification;
-//    }
-//
-//    public void setClassify_id(int classify_id) {
-//        this.classify_id = classify_id;
-//    }
-//
-//    public void setClassification(String classification) {
-//        this.classification = classification;
-//    }
 
-//    @Override
-//    public String toString() {
-//        return "Classify{" +
-//                "classify_id=" + classify_id +
-//                ", classification='" + classification + '\'' +
-//                '}';
-//    }
 }
